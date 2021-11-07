32a13871a85aedd17198e4761f48d247a66186e1
JayW <84046974+AJ-Wuu@users.noreply.github.com>
2021-11-07 14:32:56 -0600

    Update location.py

--- location.py
+++ location.py
@@ -4,8 +4,6 @@ from pandas.core import series
 import databaseQuery
 import pandas as pd
 
-businessCoordinates = []
-distances = []
 N = 20  # return the top-20 closest businesses
 
 
@@ -58,19 +56,3 @@ def locationFeatureDriver(category_1):
     for business in selectedBusiness:
         selectedBusinessInfo.append(databaseQuery.getBusinessInfo([business[0],business[1]]))
     return selectedBusinessInfo
-
-
-def main():
-    print(getCoordinates("clothes"))  # get the coordinates of all the businesses in the database
-
-    # get the distances between all the businesses and the user input (brute-force method)
-    for row in businessCoordinates:
-        getDistance(row)
-
-    # sort the distances and output the top-N closest
-    sortDistance(distances)
-    outputTopN(distances)
-
-
-if __name__ == "__main__":
-    main()
