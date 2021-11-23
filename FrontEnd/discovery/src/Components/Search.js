b127f9f231c908e9603a9c0846d70f4fcef905b6
JayW <84046974+AJ-Wuu@users.noreply.github.com>
2021-11-22 21:49:51 -0600

    Merge Codes
    Merge Codes with Main/FrontEnd/discovery/src/Components/Search.js

--- FrontEnd/discovery/src/Components/Search.js
+++ FrontEnd/discovery/src/Components/Search.js
@@ -1,10 +1,15 @@
-import React, { Component, useState } from 'react';
+import React, { useState, useEffect } from "react";
+import GoogleMapReact from "google-map-react";
 import { useLocation } from "react-router-dom";
-import axios from 'axios';
-
+import "./Search.css";
+import Marker from "../Map/Marker/Marker.jsx";
+import BusinessButton from "./BusinessButton";
+import axios from "axios";
 
 function Search() {
-    const location = useLocation();
+    const location = useLocation(); // will need to get lat and long of user from this 
+
+    const [businesses, setBusinesses] = useState(["Michael Angelo's Coffee House", "Ian's Pizza", "Fair Trade Coffee"])
     const [currentTime, setCurrentTime] = useState(0);
     const [msg, setMsg] = useState('');
 
@@ -23,12 +28,64 @@ function Search() {
        //Perform action based on error
         });
 
-
     //fetch('/time').then(res => res.json()).then(data => {
-      //  setMsg(data.time);
+    //  setMsg(data.time);
     //});
 
-    return <h2>The current time is {msg}.</h2>;
+    // to get data:
+    // const fetchPlaces = async () => {
+    //     fetch('places.json')
+    //         .then((response) => response.json())
+    //         .then((data) => setPlaces(data.results))
+    // }
+
+    // when data is updated:
+    // useEffect(() => {
+    //     fetchPlaces();
+    // }, [])
+
+    return (
+        //<h2>{location.state.params}</h2>
+        //<h2>The current time is {msg}.</h2>
+        <>
+            <div class="topnav">
+                <a class="active" href="#home">Home</a>
+            </div>
+            <div class="sidebar">
+                <div className="buttonContainer">
+                    {
+                        businesses.map((business) => (
+                            <BusinessButton
+                                text={business}
+                            />
+                        ))
+                    }
+                </div>
+            </div>
+            <div style={{ height: '100vh', width: '100%'}}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: "TO ADD" }}
+                    defaultCenter={{
+                        lat: 43.0731,
+                        lng: -89.4012
+                    }}
+                    defaultZoom={15}
+                >
+                    {
+                        // update with business objects
+                        // businesses.map((business) => (
+                        //     <Marker
+                        //         text={business.name}
+                        //         lat={business.geometry.location.lat}
+                        //         lng={business.geometry.location.lng}
+                        //     />
+                        // ))
+                    }
+                </GoogleMapReact>
+            </div>
+        </>
+
+    );
 }
 
 // exporting home component for other files to use
